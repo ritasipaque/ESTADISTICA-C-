9dd18fc0c973c066fe64b52a0618f37a68a63c1b
carlosalbertoflores <carlos.depaz1411@gmail.com>
2020-09-29 19:14:27 -0600

    first commit 4

--- proyecyo2/proyecto estadistica.cpp
+++ proyecyo2/proyecto estadistica.cpp
@@ -3,7 +3,36 @@
 using namespace std;
 
   //funciones  de  estructura//
+void ordenar (){
+ int x,a,b,aa,bb ;
+int datos[200],aux,num[200];
+int i,j;
+	cout << "ingese numero de  datos " << endl;
+	cin>>x;
+	for( i=0 ;i<x;i++){
+	    cin>>datos[i];
+	}
+	for( i=0 ;i<x;i++){
 
+   for( j=0 ;j<x;j++){
+
+ if (datos[i]<datos[j]){
+
+    aux=datos[i];
+ datos[i]=datos[j];
+  datos[j]=aux;
+ }
+ }
+ }
+
+cout<< "numeros ordenados";
+
+	for( i=0 ;i<x;i++){
+
+            cout<<datos[i];
+            }
+
+}
 void agrupados (){}
 void no_agrupados (){}
 void m_m_m (){}
